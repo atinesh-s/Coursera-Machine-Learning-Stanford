8d04cd70831b0d25f9d943ea6244c76292a54a0b
sailakshmi <sailakshmi.seemakurthi@gmail.com>
2018-10-13 16:44:25 +0530

    optimised by removing a loop

--- Week 7/Programming Assignment/machine-learning-ex6/ex6/emailFeatures.m
+++ Week 7/Programming Assignment/machine-learning-ex6/ex6/emailFeatures.m
@@ -48,10 +48,10 @@ x = zeros(n, 1);
 %
 %
 
-for i=1:size(word_indices)
-    x(word_indices(i)) = 1;
-end
-
+%for i=1:size(word_indices)
+%    x(word_indices(i)) = 1;
+%end
+x(word_indices) = 1;
 % =========================================================================
     
 end
