d9de80cc175826f4e634369371652f68fdcdcbdf
NiranjanSharma12382 <72336820+NiranjanSharma12382@users.noreply.github.com>
2022-05-09 21:57:55 +0530

    Update plotData.m

--- Week 2/Programming Assignment/machine-learning-ex1/ex1/plotData.m
+++ Week 2/Programming Assignment/machine-learning-ex1/ex1/plotData.m
@@ -1,5 +1,6 @@
 function plotData(x, y)
 %PLOTDATA Plots the data points x and y into a new figure 
+figure; % open a new figure window
 
 plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 % Hint: You can use the 'rx' option with plot to have the markers
@@ -9,6 +10,5 @@ plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
 ylabel('Profit in $10,000s'); % Set the y ? axis label
 xlabel('Population of City in 10,000s'); % Set the x ? axis label
 
-figure; % open a new figure window
 
 end
